05a3bda3dcbf881cfb48c80413d050901d3a100d
TBRC-jimk <jimk@tbrc.org>
2017-12-01 13:09:18 -0500

    Cleanup: fix indexing, remove git from Windhorse

--- DRS-BATCH-PROCESSING/testMakeDrs.sh
+++ DRS-BATCH-PROCESSING/testMakeDrs.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 #                        Input list (W,Holis pairs)     
  
- ./make-drs-batch.sh WorkSources/W1KG13999.txt BB_tbrc/BB_tbrc2drs.jsk  td3`date +%F.%H.%M` /Users/jimk/DRS BatchBuilder-2.2.11
- # ./make-drs-batch.sh WorkSources/W30013.txt BB_tbrc/BB_tbrc2drs.jsk td3GuiBuilt /home/jsk/bdrc/Test-Archive BatchBuilder2.2.11
+ ./make-drs-batch.sh HARVARD-DRS-TESTS/W1KG13999.txt BB_tbrc/BB_tbrc2drs  td3`date +%F.%H.%M` /Users/jimk/DRS BatchBuilder-2.2.11
 
 
 
