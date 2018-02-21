d483a226eb76fda1d4078eaea2b62431a475d3d4
Elie Roux <elie.roux@telecom-bretagne.eu>
2018-02-21 12:29:41 +0100

    upload compressed json on s3

--- contrib/tojsondimensions.py
+++ contrib/tojsondimensions.py
@@ -6,10 +6,15 @@ import re
 import sys
 import os
 import hashlib
+import boto3
+import gzip
 
 pathpattern = re.compile('^image/(W[A-Z0-9]+)-([A-Z0-9]+)--([A-Z0-9]+)__\d+\.([a-zA-Z0-9]+)$')
 
 outdir = 'output/'
+s3bucketName = 'archive.tbrc.org'
+s3client = boto3.resource('s3')
+s3bucket = s3client.Bucket(s3bucketName)
 
 def add_info_to_struct(path, width, height, struct):
     m = pathpattern.match(path)
@@ -47,14 +52,24 @@ def get_outfile_name(dirname):
     md5 = hashlib.md5(workid.encode('utf-8')).hexdigest()[:2]
     return outdir+md5+'_'+workid+'_'+lastdir+'_dimensions.json'
 
+def get_s3_key(dirname):
+    lastdir = dirname.split('/')[-1]
+    workid = lastdir.split('-')[0]
+    md5 = hashlib.md5(workid.encode('utf-8')).hexdigest()[:2]
+    return 'Works/'+md5+'/'+workid+'/images/'+lastdir+'/dimensions.json'
 
 def build_lists_from_dir(dirname):
     for root, batchdirnames, _ in os.walk(dirname):
         if root.count(os.path.sep) == 2: # yeark...
-            outfilename = get_outfile_name(root)
+            key = get_s3_key(root)
             list = get_list_from_file(root+'/descriptor.xml')
-            with open(outfilename, 'w') as outfile:
-                json.dump(list, outfile)
+            jsonstr = json.dumps(list)
+            # with open(get_outfile_name(root), 'w') as outfile:
+            #     json.dump(list, outfile)
+            json_data = bytes(jsonstr, 'utf-8')
+            gzip_data = gzip.compress(json_data)
+            print("uploading %s" % key)
+            s3bucket.put_object(Key=key, Body=gzip_data, ContentType='application/json', ContentEncoding='gzip')
 
 #struct = get_list_from_file("METS-descriptors-W22084-22703-22704/batchW22084-1/W22084-0886/descriptor.xml")
 build_lists_from_dir("METS-descriptors-W22084-22703-22704")
