e24c49f3d63aed693e9dc30008f8a6ad6ceb5c1a
TBRC-jimk <jimk@tbrc.org>
2018-02-22 11:04:33 -0500

    Put ftpMuliple.sh in right home

--- DRS-BATCH-PROCESSING/parallelBatch/ftpMultiple.sh
+++ DRS-BATCH-PROCESSING/parallelBatch/ftpMultiple.sh
@@ -16,33 +16,137 @@
 #
 # Some constants
 #
-WORKS_LIST_PATH=lSrc/worksList
+UNDERWAY_DIR=timing/underway
+[ -d  $UNDERWAY_DIR ] || mkdir -p $UNDERWAY_DIR
+
+RESULTS_DIR=timing/finishedRuns
+[ -d  $RESULTS_DIR ] || mkdir -p $RESULTS_DIR
+
+NO_VALUE='$@@<<no_value>>@@'
+#end constants
 #
 
 ME=$(basename $0)
 
+#----------------------------------------------------------------------
+# Usage
+#
 function usage() {
 	cat << USAGE
-		synopsis: $ME wl1 wl2[=wl1]
-		run multiple lists of works in files '${'WORKS_SRC'}'/worksList\$wl1 ... \$wl2.txt
-		wl2 defaults to wl1 if not given.
-		wl2 can be less than wl1 - bash seq is the iterator
+		synopsis: $ME [ -u userFileName ] -w worksListPath -h
+		synopsis: $ME  wl1 wl2[=wl1] ]  -w worksListPath -h
+				
+		run multiple lists of works in files '${'worksListPath}{1..n}.txt
+
+		-u userFileName [optional] is a list of users of the host.
+			This list controls a splitting of the works list into
+			one file for each user, to support multiple parallel transfers
+			to multiple accounts
+
+			if userFileName is given, the argument to the 
+			-w flag represents a single file which contains all the paths
+			to be sent up. $ME divides the works list into one file 
+			for each user in userFileName, and numbers them
+			(dirname worksListPath)/basename worksListPath{1..n}.txt
+
+			if the two arguments wl1 [ wl2 ] are given, and the -u flag is
+			NOT given, then wl1 and wl2 stand for a range of integers which 
+			defines a set of files, and the -w option is a pattern consisting
+			of "directory name"/prefix "directory name" is created if it does
+			not exist. The files are created in the current directory if 
+			worksListPath has no directory component. $ME sends the files
+			worksListPath{1..n}.txt to the makeOneFtp.sh
+
+		wl1, wl2: When the -u flag is not given, integers which define the range 
+			of workLists to be parallel streamed. wl2 (worksList defaults to the
+			value of wl1 if not given.wl2 can be less than wl1:
+			bash seq is the iterator
 USAGE
 }
 
-underwayDir=timing/underway
-[ -d  $underwayDir ] || mkdir -p $underwayDir
+#----------------------------------------------------------------------
+# Noisily expire
 #
-resultsDir=timing/finishedRuns
-[ -d  $resultsDir ] || mkdir -p $resultsDir
+function die() {
+	rc=$1
+	shift 1
+	echo $@
+	exit $rc
+}
+
+#----------------------------------------------------------------------
+#
+# use the flags to determine what's in the workslist
+#
+# Args: Uses the outer scope's variables:
+# Reads:  	userList (file)
+#			worksListPath (source of works)
+#
+# Writes:
+#	in $worksListPath directory, $worksListPath (without file extension 1..n)
+# 	One file for each user line in userList.
+#
+#   Populates:
+#		the outer scope's sendUsers array
+#		the outer scope's $wl1 and $wl2 indices (the limits of the)
+# 
+function splitWorks() {
+	worksDir=$(dirname $worksListPath)
+	worksName=$(basename $worksListPath)
+	worksSuffix=${worksName##*.}
+
+	while read aUser ; do
+		sendingUsers+=$aUser
+	done < $userListPath
+
+
+}
+
+#
+# h   elp
+# u   serLost
+# w   orksListPath
+while getopts hu:w: opt ; do
+	case $opt in
+		u)
+
+			userListPath=$OPTARG
+			[ -f $userListPath ] || { die 2 "${ME}:error:List of users file \'$userListPath\' must exist but does not"}
+			;;
+		w)
+			worksListPath=$OPTARG;
+			;;
+		h)
+			usage
+			exit 0
+			;;
+	esac
+done
+shift $((OPTIND-1))
 
 # if no args, bail
-[ x"$1" == "x" ] && { usage ; exit 1 ; }
 
-wl1=$1
-wl2=$2
+# if we dont have a user list, and we dont have any args,
+# we're toast, and we're outta here
 
-[ x"$wl2" == "x" ] && { usage ; printf '\n** running one ** %s\n' $wl1 ; wl2=$wl1 ; }
+[ "${userList:-$NO_VALUE}"  == "$NO_VALUE" ] &&  [ "${1:-$NO_VALUE}"  == "$NO_VALUE" ] &&  ${ usage ; exit 1 ; }
+
+# worksListPath required
+worksListPath=${worksListPath:?${ME}:usage:worksListPath is empty, or -w flag not given}
+
+
+if "${userList:-$NO_VALUE}"  !=  "$NO_VALUE" ; then
+# splitWorks fills this in
+	sendingUsers=()
+	splitWorks()
+else
+
+# Set default for wl2 if not given
+	wl1=$1
+	wl2=${2:-$wl1}
+fi
+
+# 
 
 for x in $(seq $wl1 $wl2 ); do
 	#
@@ -52,6 +156,6 @@ for x in $(seq $wl1 $wl2 ); do
 	# Run each iteration in the background
 	# jsk 21.I.18: shell scripts can be in ~/bin
         
-	makeOneFtp.sh ${WORKS_LIST_PATH}${x}.txt $underwayDir $resultsDir &
+	makeOneFtp.sh ${worksListPath}${x}.txt $UNDERWAY_DIR $RESULTS_DIR &
  
 done
