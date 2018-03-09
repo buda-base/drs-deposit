38b360f026395dd38b7254334287cc99563376c2
Jim Katz <jimk@tbrc.org>
2018-03-09 16:13:19 -0500

    Send database to RDSAWS

---
+++ database/scripts/DumpDRS.ps1
@@ -0,0 +1,9 @@
+﻿#
+# Dump drs. 
+# Add routines
+mysqldump --login-path=ubu1610 --opt --routines --databases drs --result-file=ubu1610.dump.sql
+
+#
+# To reload, 
+# mysql ----
+# mysql> source whatever
