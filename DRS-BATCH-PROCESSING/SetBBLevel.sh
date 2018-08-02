d2a105e5bf1ffc345b70358c191e74dc32f1ff02
Jim Katz <jimk@tbrc.org>
2018-08-02 17:19:59 -0400

    WIP injection test harness

--- DRS-BATCH-PROCESSING/SetBBLevel.sh
+++ DRS-BATCH-PROCESSING/SetBBLevel.sh
@@ -1,9 +1,22 @@
+#!/usr/bin/env bash
 # Bash source file to set Batchbuilder level
 # Usage . <wherever>/SetBBLevel.sh
 # These are the only two values.
 # they must be a suffix of one of the file 
 # in $BB_HOME/conf/bb.properties
 # Case sensitive
-export BB_LEVEL=prod
-# export BB_LEVEL=qa
+#
+# make-drs-batch.sh stops unless this value is set
+export PROD_BB_LEVEL=prod
+export QA_BB_LEVEL=qa
 
+
+export BB_LEVEL=$PROD_BB_LEVEL
+
+# Set up the nrs resolver for injecting into project.conf
+# See DRS-BATCH-PROCESSING/make-drs-batch.conf
+
+prodNRS=https://nrs.lib.harvard.edu/
+qaNRS=https://nrs-qa.lib.harvard.edu/
+
+export HUL_NRS_RESOLVER_URL=${BB_LEVEL}NRS
---
+++ RelatedURNInject/bin/InjectOutlineId.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# Arrange mock
+MEPATH=$dd/RelatedURNInject/bin
+export outPath=$dd/RelatedURNInject/output/
+export HID="HOLLISIDPARAM"
+export targetConf="Injected.xml"
+export masterProjConf="${MEPATH}/project.conf"
+
+# This is a scrape of SetBBLevel.sh for qa
+#################################################
+export PROD_BB_LEVEL=prod
+export QA_BB_LEVEL=qa
+
+
+export BB_LEVEL=$QA_BB_LEVEL
+
+# Set up the nrs resolver for injecting into project.conf
+# See DRS-BATCH-PROCESSING/make-drs-batch.conf
+
+prodNRS=https://nrs.lib.harvard.edu/
+qaNRS=https://nrs-qa.lib.harvard.edu/
+
+export HUL_NRS_RESOLVER_URL=${BB_LEVEL}NRS
+##################################################
+
+export iCt=0
+
+while IFS=: read -ra lineArgs ; do
+    printMasterURI=
+    outlineURI=
+    [ ! -z "${lineArgs[0]}" ] && { outlineURI=${HUL_NRS_RESOLVER_URL}${lineArgs[0]} ; } ||
+    [ ! -z "${lineArgs[1]}" ] && { printMasterURI=${HUL_NRS_RESOLVER_URL}${lineArgs[1]} ; }
+
+    ((iCt++))
+    echo   ${outPath}${iCt}${targetConf}
+    java -jar "${MEPATH}/saxonhe-9.4.0.7.jar" ${masterProjConf} ${MEPATH}/make-proj-conf.xsl outlineURI=${outlineURI}  printMasterURI=${printMasterURI} hId=${HID}  > ${outPath}${iCt}${targetConf}
+done << YOW
+outline1:pm1
+outlineOnly
+:pmOnly
+
+YOW
